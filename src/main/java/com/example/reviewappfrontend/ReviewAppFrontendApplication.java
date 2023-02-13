6c1629c00fb3d31ed6e0a4c9fdd0be0e24683ad8
radumbaciu <radumbaciu@gmail.com>
2023-02-13 19:16:50 +0200

    initial commit

---
+++ src/main/java/com/example/reviewappfrontend/ReviewAppFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.reviewappfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewAppFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewAppFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reviewappfrontend/ReviewAppFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reviewappfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewAppFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
